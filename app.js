79c085c269946d062830d902c21d9847a91325ab
Olayemi Adebayo <techwriter2022@gmail.com>
2020-02-24 14:54:41 +0100

    added the map and layouts

---
+++ app.js
@@ -0,0 +1,124 @@
+$(document).ready(function() {
+  let restaurantList = [
+    {
+      restaurantName: "Bronco",
+
+      address: "39 Rue des Petites Écuries, 75010 Paris",
+      lat: -32,
+
+      long: 150,
+
+      ratings: [
+        {
+          stars: 4,
+
+          comment: "Great! But not many veggie options."
+        },
+
+        {
+          stars: 5,
+
+          comment: "My favorite restaurant!"
+        }
+      ]
+    },
+    {
+      restaurantName: "Bar beach",
+
+      address: "39 Rue des Petites Écuries, 75010 Paris",
+      lat: -32,
+
+      long: 150,
+
+      ratings: [
+        {
+          stars: 4,
+
+          comment: "Great! But not many veggie options."
+        },
+
+        {
+          stars: 5,
+
+          comment: "My favorite restaurant!"
+        }
+      ]
+    },
+
+    {
+      restaurantName: "Babalou",
+
+      address: "4 Rue Lamarck, 75018 Paris",
+      lat: -30.5,
+
+      long: 152,
+
+      ratings: [
+        {
+          stars: 5,
+
+          comment: "Tiny pizzeria next to Sacre Coeur!"
+        },
+
+        {
+          stars: 3,
+
+          comment: "Meh, it was fine."
+        }
+      ]
+    },
+
+    {
+      restaurantName: "Dominos",
+
+      address: "33, Palm Avenue, Victoria",
+      lat: -29.5,
+
+      long: 151,
+
+      ratings: [
+        {
+          stars: 2,
+
+          comment: "The Best Ever"
+        },
+
+        {
+          stars: 3,
+
+          comment: "Will come again"
+        }
+      ]
+    }
+  ];
+
+  function card(restaurant) {
+    $("#restaurant-card").append(`<div class="col-md-8">
+    <div class="card border-info mb-2 breadth">
+   <div class="card-header alert alert-info" id="name">${restaurant.restaurantName}</div>
+   <div class="card-body text-info">
+     <h5 class="card-title" id="address">${restaurant.address}</h5>
+     <p class="card-text" id="rating">${restaurant.ratings[0].stars}</p>  
+   </div>
+ </div>
+</div>
+<div class="col-md-4"><img src="jonathan-borba-5E0d3lfoC1w-unsplash.jpg" id="radius" class="card-img"alt="Restaurant image"/></div>`);
+  }
+
+  for (let i = 0; i < restaurantList.length; i++) {
+    card(restaurantList[i]);
+  }
+});
+
+function initMap() {
+  // The location of myPosition
+  var myPosition = {lat: 6.548287999999, lng: 3.363564299999};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 16, center: myPosition});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: myPosition, map: map});
+
+  
+}
+
---
+++ map.js
@@ -0,0 +1,12 @@
+function initMap() {
+  // The location of myPosition
+  var myPosition = {lat: 6.548287999999, lng: 3.363564299999};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 16, center: myPosition});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: myPosition, map: map,draggable: false});
+}
+
+
+
