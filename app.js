3acdd362611947b27335726a07523ce051c43b89
Olayemi Adebayo <techwriter2022@gmail.com>
2020-03-30 13:19:46 +0100

    added the hash sign within href

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ function card(restaurant, id) {
   $("#restaurant-card").append(`
   <div class="col-md-8">
     <div class="card border-info mb-2 breadth">
-      <div class="card-header alert alert-info" id="name_${id}"><a href="" onclick="commentModal()">${restaurant.restaurantName}</a></div>
+      <div class="card-header alert alert-info" id="name_${id}"><a href="#" onclick="commentModal()">${restaurant.restaurantName}</a></div>
     <div class="card-body text-info">
       <h5 class="card-title" id="address_${id}">${restaurant.address}</h5>
       <input class="rating" data-readonly="true" id="rating_${id}" value="${average}" />  
